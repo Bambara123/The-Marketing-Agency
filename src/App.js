aaf70ed7912bb9b1ab53d11887ad8861372083f3
Bambara123 <e19034@eng.pdn.ac.lk>
2024-06-21 07:07:21 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import "./App.css";
-import colors from "./config/colors";
+
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <div className="zen-antque-regular" style={{ color: colors.green }}>
-        This is ultra
-      </div>
-
-      <div className="red-hat-text">This is ultra</div>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,56 @@
 import React from "react";
 import "./Header.css";
+import { useEffect } from "react";
 
 export default function () {
-  return <div className="app-header">Header</div>;
+  useEffect(() => {
+    const image = document.querySelectorAll(".actual_file");
+    const timer = setTimeout(() => {
+      image.forEach((element) => {
+        element.setAttribute(
+          "src",
+          process.env.PUBLIC_URL + "/background_video.gif"
+        );
+      });
+    }, 1000);
+
+    // Cleanup function to clear the timeout if the component unmounts before the timeout finishes
+    return () => clearTimeout(timer);
+  }, []); // Empty dependency array means this effect runs once on mount and cleans up on unmount
+  return (
+    <div class="header">
+      <div class="bg_video">
+        <img
+          class="actual_file"
+          src={process.env.PUBLIC_URL + "/background_video.png"}
+          alt=""
+        />
+        <div class="filter-blur"></div>
+      </div>
+      <div class="bg_video_masked">
+        <img
+          src={process.env.PUBLIC_URL + "/background_video.png"}
+          alt=""
+          class="circle_mask actual_file"
+        />
+      </div>
+      <h1 class="title">
+        The <br />
+        Marketing <br />
+        Agency
+      </h1>
+      <div class="invert">
+        <img
+          src={process.env.PUBLIC_URL + "/background_video.png"}
+          alt=""
+          class="actual_file"
+        />
+        <h1 class="title">
+          The <br />
+          Marketing <br />
+          Agency
+        </h1>
+      </div>
+    </div>
+  );
 }
